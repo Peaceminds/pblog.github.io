56b551a29465d2c691e83577a912d2c6531106e8
Peaceminds <849271631@qq.com>
2020-05-24 16:18:47 +0800

    gh-pages branch.

--- src/routers.js
+++
@@ -1,9 +0,0 @@
-import ShowBlogs from './components/ShowBlogs'
-import AddBlogs from './components/AddBlog'
-import SingleBlog from './components/SingleBlog'
-
-export default[
-    {path:"/", component:ShowBlogs},
-    {path:"/add", component:AddBlogs},
-    {path:"/bl/:id", component:SingleBlog}
-]
